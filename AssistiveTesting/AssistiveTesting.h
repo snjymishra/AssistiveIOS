1387d5d675fd81805504be29c40474f7ef984754
Sanjay Kumar Mishra <sanjay.mishra@daffodilsw.com>
2025-01-28 12:02:07 +0530

    testing framework

---
+++ AssistiveTesting/AssistiveTesting.h
@@ -0,0 +1,18 @@
+//
+//  AssistiveTesting.h
+//  AssistiveTesting
+//
+//  Created by Sanjay on 28/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssistiveTesting.
+FOUNDATION_EXPORT double AssistiveTestingVersionNumber;
+
+//! Project version string for AssistiveTesting.
+FOUNDATION_EXPORT const unsigned char AssistiveTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssistiveTesting/PublicHeader.h>
+
+
